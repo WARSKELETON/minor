d6b34eada18da3873967c6d52d35efa26ed25895
Unknown <yugi.man22@gmail.com>
2020-05-21 13:04:50 +0100

    Reverted

--- minor2/out.asm
+++ minor2/out.asm
@@ -3,21 +3,70 @@ segment	.text
 ; ALIGN
 align	4
 ; GLOBL
-global	$_double:function
+global	$_fib:function
 ; LABEL
-$_double:
+$_fib:
 ; ENTER
 	push	ebp
 	mov	ebp, esp
 	sub	esp, 0
-; ADDRV
-	push	dword [$y]
+; LOCV
+	push	dword [ebp+8]
 ; IMM
 	push	dword 2
-; MUL
+; LT
 	pop	eax
-	imul	dword eax, [esp]
-	mov	[esp], eax
+	xor	ecx, ecx
+	cmp	[esp], eax
+	setl	cl
+	mov	[esp], ecx
+; JZ
+	pop	eax
+	cmp	eax, byte 0
+	je	near $_L1
+; LOCV
+	push	dword [ebp+8]
+; POP
+	pop	eax
+; LEAVE
+	leave
+; RET
+	ret
+; JMP
+	jmp	dword $_L2
+; LABEL
+$_L1:
+; LABEL
+$_L2:
+; LOCV
+	push	dword [ebp+8]
+; IMM
+	push	dword 1
+; SUB
+	pop	eax
+	sub	dword [esp], eax
+; CALL
+	call	$_fib
+; TRASH
+	add	esp, 4
+; PUSH
+	push	eax
+; LOCV
+	push	dword [ebp+8]
+; IMM
+	push	dword 2
+; SUB
+	pop	eax
+	sub	dword [esp], eax
+; CALL
+	call	$_fib
+; TRASH
+	add	esp, 4
+; PUSH
+	push	eax
+; ADD
+	pop	eax
+	add	dword [esp], eax
 ; POP
 	pop	eax
 ; LEAVE
@@ -40,10 +89,124 @@ $_main:
 	push	ebp
 	mov	ebp, esp
 	sub	esp, 0
-; IMM
-	push	dword 4
+; RODATA
+segment	.rodata
+; ALIGN
+align	4
+; LABEL
+$_L3:
+; CHAR
+	db	0x51
+; CHAR
+	db	0x75
+; CHAR
+	db	0x61
+; CHAR
+	db	0x6C
+; CHAR
+	db	0x20
+; CHAR
+	db	0x6F
+; CHAR
+	db	0x20
+; CHAR
+	db	0x74
+; CHAR
+	db	0x65
+; CHAR
+	db	0x72
+; CHAR
+	db	0x6D
+; CHAR
+	db	0x6F
+; CHAR
+	db	0x20
+; CHAR
+	db	0x64
+; CHAR
+	db	0x61
+; CHAR
+	db	0x20
+; CHAR
+	db	0x73
+; CHAR
+	db	0xC3
+; CHAR
+	db	0xA9
+; CHAR
+	db	0x72
+; CHAR
+	db	0x69
+; CHAR
+	db	0x65
+; CHAR
+	db	0x20
+; CHAR
+	db	0x64
+; CHAR
+	db	0x65
+; CHAR
+	db	0x20
+; CHAR
+	db	0x46
+; CHAR
+	db	0x69
+; CHAR
+	db	0x62
+; CHAR
+	db	0x6F
+; CHAR
+	db	0x6E
+; CHAR
+	db	0x61
+; CHAR
+	db	0x63
+; CHAR
+	db	0x63
+; CHAR
+	db	0x69
+; CHAR
+	db	0x20
+; CHAR
+	db	0x61
+; CHAR
+	db	0x20
+; CHAR
+	db	0x63
+; CHAR
+	db	0x61
+; CHAR
+	db	0x6C
+; CHAR
+	db	0x63
+; CHAR
+	db	0x75
+; CHAR
+	db	0x6C
+; CHAR
+	db	0x61
+; CHAR
+	db	0x72
+; CHAR
+	db	0x3A
+; CHAR
+	db	0x20
+; CHAR
+	db	0x00
+; TEXT
+segment	.text
+; ADDR
+	push	dword $_L3
 ; CALL
-	call	$_double
+	call	$_prints
+; TRASH
+	add	esp, 4
+; CALL
+	call	$_readi
+; PUSH
+	push	eax
+; CALL
+	call	$_fib
 ; TRASH
 	add	esp, 4
 ; PUSH
@@ -57,7 +220,7 @@ segment	.rodata
 ; ALIGN
 align	4
 ; LABEL
-$_L1:
+$_L4:
 ; CHAR
 	db	0x0A
 ; CHAR
@@ -65,7 +228,7 @@ $_L1:
 ; TEXT
 segment	.text
 ; ADDR
-	push	dword $_L1
+	push	dword $_L4
 ; CALL
 	call	$_prints
 ; TRASH
